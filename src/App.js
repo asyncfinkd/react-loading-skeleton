4cdad61c636ebeb23f0d1233657f8a67017b675b
asyncfinkd <shamgoingzuck@gmail.com>
2021-05-02 01:11:12 +0400

    :rocket: Header

--- src/App.js
+++ src/App.js
@@ -3,7 +3,24 @@ import React from "react";
 export default function App() {
   return (
     <>
-      <p>Hello</p>
+      <div>
+        <header>
+          <div>
+            <h1>React Loading Skeleton</h1>
+          </div>
+          <div>
+            <h2
+              style={{ cursor: "pointer" }}
+              onClick={() => {
+                window.location.href = "https://github.com/asyncfinkd";
+              }}
+            >
+              asyncfinkd
+            </h2>
+          </div>
+        </header>
+        <div className="content"></div>
+      </div>
     </>
   );
 }
