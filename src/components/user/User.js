ae99701503f4f7802d583fd9f6bf61faf6314820
asyncfinkd <shamgoingzuck@gmail.com>
2021-05-02 14:09:35 +0400

    :rocket: Data give

--- src/components/user/User.js
+++ src/components/user/User.js
@@ -5,7 +5,7 @@ export default function User() {
 
   useEffect(() => {
     setTimeout(async () => {
-      const res = await fetch("https://jsonplaceholder.typicode.com/users");
+      const res = await fetch("https://jsonplaceholder.typicode.com/users/1");
       const data = await res.json();
       setProfile(data);
     }, 5000);
@@ -15,6 +15,13 @@ export default function User() {
     <>
       <div className="user">
         <h2>User</h2>
+        {profile && (
+          <div className="profile">
+            <h3>{profile.username}</h3>
+            <p>{profile.email}</p>
+            <a href={profile.website}>{profile.website}</a>
+          </div>
+        )}
       </div>
     </>
   );
